9a3b54fb6294b9ed1d14a64bd3d394687aac9048
CeliaCha <chazel.celia@gmail.com>
2018-01-04 13:19:56 +0100

    ++ Localstorage, gestion des dates et Delete

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,78 +1,92 @@
-var taskList = [];
 
-// Listener du bouton ajouter
+var taskStorage = localStorage;
+if (JSON.parse(taskStorage.getItem("userStorage")) === null) { var taskArray = []; }
+else { var taskArray = JSON.parse(taskStorage.getItem('userStorage')); }
+
+
+window.onload = function() {
+  document.getElementById('viewTasks').innerHtml = "";
+  // log(taskArray);
+    for (index in taskArray) {
+      var taskName = taskArray[index].name;
+      addTask(taskName);
+    }
+}
+
+// LISTENERS
 document.getElementById("submitTask").addEventListener("click", addTask);
-
-// Listeners boutons radio du filtre
 document.getElementById("todo").addEventListener("click", displayToDo);
 document.getElementById("done").addEventListener("click", displayDone);
 document.getElementById("alltasks").addEventListener("click", displayAll);
 
-function displayToDo() {
-  displayAll();
-  if (this.checked) {
-    for (i in taskList) {
-      var myTask = document.getElementById('label' + taskList[i].name);
-      if (myTask.style.textDecoration == "line-through") {
-        myTask.style.display = "none";
-      }
-    }
+
+// APPELÉE PAR BOUTON AJOUTER
+function addTask(taskName) {
+  // création objet Task s'il n'existe pas déjà
+  if (this.id == 'submitTask') {
+    var taskName = document.getElementById("defineTask").value;
+    var newTask = new Task(taskName, new Date(), "Simplon");
+    taskArray.push(newTask);
+    updateLocalStorage();
   }
-}
-
-function displayDone() {
-  displayAll();
-  if (this.checked) {
-    for (i in taskList) {
-      var myTask = document.getElementById('label' + taskList[i].name);
-      if (myTask.style.textDecoration != "line-through") {
-        myTask.style.display = "none";
-      }
-    }
+  else {
+    var newTask = findTaskByName(taskName);
   }
-}
-
-function displayAll() {
-  for (i in taskList) {
-    var myTask = document.getElementById('label' + taskList[i].name);
-    myTask.style.display = "inline";
-  }
-}
-
-// appelée par bouton ajouter
-function addTask() {
-  // ajout objet Task
-  var taskName = document.getElementById("defineTask").value;
-  var newTask = new Task(taskName, new Date(), "Simplon");
-  taskList.push(newTask);
-
   // ajout dans le DOM
   var li = document.createElement('li');
   var label = document.createElement('label');
   var checkbox = document.createElement('input');
+  var button = document.createElement('button');
+  li.setAttribute('id', 'li-' + newTask.name);
   checkbox.setAttribute('type', 'checkbox');
-  checkbox.setAttribute('id', newTask.name);
-  label.setAttribute('for', newTask.name);
-  label.setAttribute('id', 'label' + newTask.name);
-  label.textContent = newTask.name;
+  checkbox.setAttribute('id', 'checkbox-' + newTask.name);
+  label.setAttribute('for', 'checkbox-' + newTask.name);
+  label.setAttribute('id', 'label-' + newTask.name);
+  label.textContent = newTask.name + " (" + newTask.date.toLocaleDateString() + " à " + newTask.date.toLocaleTimeString()+ ")";
+  button.setAttribute('id', 'button-' + newTask.name);
+  button.textContent = "Delete";
   li.appendChild(checkbox);
   li.appendChild(label);
-  document.getElementById("tasklist").appendChild(li);
-
+  li.appendChild(button);
+  document.getElementById("viewTasks").appendChild(li);
   // Ajoute un listener sur chaque nouveau checkbox
-  var myCheckbox = document.getElementById(newTask.name);
+  var myCheckbox = document.getElementById('checkbox-' + newTask.name);
   myCheckbox.addEventListener("click", updateTasks);
+  // Ajoute un listener sur chaque nouveau bouton delete
+  var myDeleteButton = document.getElementById('button-' + newTask.name);
+  myDeleteButton.addEventListener("click", deleteTask); 
 }
 
+function deleteTask() {
+  if (confirm('Supprimer cette tâche ?')) {
+    var taskToDelete = findTaskByName(this.id.split("-").splice(-1).toString()); 
+    taskArray.splice(taskArray.indexOf(taskToDelete), 1);
+    var liToDelete = document.getElementById('li-' + taskToDelete.name);
+    liToDelete.remove();
+    updateLocalStorage();
+  }
+}
 
-// appelée quand une checkbox est cochée
+// APPELLÉE PAR CHECKBOXES
 function updateTasks() {
-  var thisLabel = document.getElementById('label' + this.id);
+  var taskToUpdate = findTaskByName(this.id.split("-").splice(-1).toString());
+  log(taskToUpdate);
+  if (taskToUpdate.isDone == false) {
+    taskToUpdate.isDone = true;
+    var dateDiff = msToHMS((Date.now() - taskToUpdate.date.getTime()));
+    alert(dateDiff);
+  }
+  else {
+    taskToUpdate.isDone = false;
+  }
+  // raye ou déraye les tâches 
+  var thisLabel = document.getElementById('label-' + taskToUpdate.name);
   thisLabel.style.textDecoration == "line-through" ? thisLabel.style.textDecoration = "none" : thisLabel.style.textDecoration = "line-through";
 }
 
 
-// Objet Task
+
+// OBJET TASK
 var Task = function(name, date, category) {
   this.name = name;
   this.date = date;
@@ -81,7 +95,74 @@ var Task = function(name, date, category) {
 }
 
 
+// FILTRES DES TÂCHES
+function displayToDo() {
+  displayAll();
+  if (this.checked) {
+    for (i in taskArray) {
+      var myTask = document.getElementById('label-' + taskArray[i].name);
+      if (myTask.style.textDecoration == "line-through") {
+        myTask.style.display = "none";
+      }
+    }
+  }
+}
+function displayDone() {
+  displayAll();
+  if (this.checked) {
+    for (i in taskArray) {
+      var myTask = document.getElementById('label-' + taskArray[i].name);
+      if (myTask.style.textDecoration != "line-through") {
+        myTask.style.display = "none";
+      }
+    }
+  }
+}
+function displayAll() {
+  for (i in taskArray) {
+    var myTask = document.getElementById('label-' + taskArray[i].name);
+    myTask.style.display = "inline";
+  }
+}
+
+
+// ACTUALISATION LOCALSTORAGE
+function updateLocalStorage() {
+  if (taskArray.length != 0) {taskStorage.setItem('userStorage', JSON.stringify(taskArray));}
+  else {taskStorage.removeItem('userStorage');}
+}
+
+
+
+// CONVERSION MILLISECONDES EN FORMAT DATE
+function msToHMS(ms) {
+    // 1- Convert to seconds:
+    var seconds = ms / 1000;
+    // 2- Extract hours:
+    var hours = parseInt( seconds / 3600 ); // 3,600 seconds in 1 hour
+    seconds = seconds % 3600; // seconds remaining after extracting hours
+    // 3- Extract minutes:
+    var minutes = parseInt( seconds / 60 ); // 60 seconds in 1 minute
+    // 4- Keep only seconds not extracted to minutes:
+    seconds = Math.round(seconds % 60);
+    return ("Vous avez réalisé cette tâche en "+ hours+" heures, "+minutes+" minutes et "+seconds+" secondes.");
+  } 
+
+// RECHERCHE OBJET PAR PROPRIÉTÉ NOM
+function findTaskByName(nameTask) {
+  // mise à jour des propriétés de l'objet Task
+  function findTask(thisTask) { 
+    return thisTask.name == nameTask;
+  }
+  return taskArray.find(findTask);
+}
+
+
+
+
 //DEV
 function log(truc) {
   console.log(truc);
 }
+
+
--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,166 @@
-function displayToDo(){if(displayAll(),this.checked)for(i in taskList){var e=document.getElementById("label"+taskList[i].name);"line-through"==e.style.textDecoration&&(e.style.display="none")}}function displayDone(){if(displayAll(),this.checked)for(i in taskList){var e=document.getElementById("label"+taskList[i].name);"line-through"!=e.style.textDecoration&&(e.style.display="none")}}function displayAll(){for(i in taskList){document.getElementById("label"+taskList[i].name).style.display="inline"}}function addTask(){var e=document.getElementById("defineTask").value,t=new Task(e,new Date,"Simplon");taskList.push(t);var n=document.createElement("li"),i=document.createElement("label"),a=document.createElement("input");a.setAttribute("type","checkbox"),a.setAttribute("id",t.name),i.setAttribute("for",t.name),i.setAttribute("id","label"+t.name),i.textContent=t.name,n.appendChild(a),n.appendChild(i),document.getElementById("tasklist").appendChild(n);document.getElementById(t.name).addEventListener("click",updateTasks)}function updateTasks(){var e=document.getElementById("label"+this.id);"line-through"==e.style.textDecoration?e.style.textDecoration="none":e.style.textDecoration="line-through"}function log(e){console.log(e)}var taskList=[];document.getElementById("submitTask").addEventListener("click",addTask),document.getElementById("todo").addEventListener("click",displayToDo),document.getElementById("done").addEventListener("click",displayDone),document.getElementById("alltasks").addEventListener("click",displayAll);var Task=function(e,t,n){this.name=e,this.date=t,this.category=n,this.isDone=!1};
+
+var taskStorage = localStorage;
+if (JSON.parse(taskStorage.getItem("userStorage")) === null) { var taskArray = []; }
+else { var taskArray = JSON.parse(taskStorage.getItem('userStorage')); }
+
+
+window.onload = function() {
+  document.getElementById('viewTasks').innerHtml = "";
+    for (index in taskArray) {
+      var taskName = taskArray[index].name;
+      addTask(taskName);
+    }
+}
+
+// LISTENERS
+document.getElementById("submitTask").addEventListener("click", addTask);
+document.getElementById("todo").addEventListener("click", displayToDo);
+document.getElementById("done").addEventListener("click", displayDone);
+document.getElementById("alltasks").addEventListener("click", displayAll);
+
+
+// APPELÉE PAR BOUTON AJOUTER
+function addTask(taskName) {
+  // création objet Task s'il n'existe pas déjà
+  if (this.id == 'submitTask') {
+    var taskName = document.getElementById("defineTask").value;
+    var newTask = new Task(taskName, new Date(), "Simplon");
+    taskArray.push(newTask);
+    updateLocalStorage();
+  }
+  else {
+    var newTask = findTaskByName(taskName);
+  }
+  // ajout dans le DOM
+  var li = document.createElement('li');
+  var label = document.createElement('label');
+  var checkbox = document.createElement('input');
+  var button = document.createElement('button');
+  li.setAttribute('id', 'li-' + newTask.name);
+  checkbox.setAttribute('type', 'checkbox');
+  checkbox.setAttribute('id', 'checkbox-' + newTask.name);
+  label.setAttribute('for', 'checkbox-' + newTask.name);
+  label.setAttribute('id', 'label-' + newTask.name);
+  label.textContent = newTask.name + " (" + new Date(newTask.date).toLocaleDateString() + " à " + new Date(newTask.date).toLocaleTimeString()+ ")";
+  button.setAttribute('id', 'button-' + newTask.name);
+  button.textContent = "Delete";
+  li.appendChild(checkbox);
+  li.appendChild(label);
+  li.appendChild(button);
+  document.getElementById("viewTasks").appendChild(li);
+  // Ajoute un listener sur chaque nouveau checkbox
+  var myCheckbox = document.getElementById('checkbox-' + newTask.name);
+  myCheckbox.addEventListener("click", updateTasks);
+  // Ajoute un listener sur chaque nouveau bouton delete
+  var myDeleteButton = document.getElementById('button-' + newTask.name);
+  myDeleteButton.addEventListener("click", deleteTask); 
+}
+
+function deleteTask() {
+  if (confirm('Supprimer cette tâche ?')) {
+    var taskToDelete = findTaskByName(this.id.split("-").splice(-1).toString()); 
+    taskArray.splice(taskArray.indexOf(taskToDelete), 1);
+    var liToDelete = document.getElementById('li-' + taskToDelete.name);
+    liToDelete.remove();
+    updateLocalStorage();
+  }
+}
+
+// APPELLÉE PAR CHECKBOXES
+function updateTasks() {
+  var taskToUpdate = findTaskByName(this.id.split("-").splice(-1).toString());
+  log(taskToUpdate);
+  if (taskToUpdate.isDone == false) {
+    taskToUpdate.isDone = true;
+    var dateDiff = msToHMS((Date.now() - taskToUpdate.date.getTime()));
+    alert(dateDiff);
+  }
+  else {
+    taskToUpdate.isDone = false;
+  }
+  // raye ou déraye les tâches 
+  var thisLabel = document.getElementById('label-' + taskToUpdate.name);
+  thisLabel.style.textDecoration == "line-through" ? thisLabel.style.textDecoration = "none" : thisLabel.style.textDecoration = "line-through";
+}
+
+
+
+// OBJET TASK
+var Task = function(name, date, category) {
+  this.name = name;
+  this.date = date;
+  this.category = category;
+  this.isDone = false;
+}
+
+
+// FILTRES DES TÂCHES
+function displayToDo() {
+  displayAll();
+  if (this.checked) {
+    for (i in taskArray) {
+      var myTask = document.getElementById('label-' + taskArray[i].name);
+      if (myTask.style.textDecoration == "line-through") {
+        myTask.style.display = "none";
+      }
+    }
+  }
+}
+function displayDone() {
+  displayAll();
+  if (this.checked) {
+    for (i in taskArray) {
+      var myTask = document.getElementById('label-' + taskArray[i].name);
+      if (myTask.style.textDecoration != "line-through") {
+        myTask.style.display = "none";
+      }
+    }
+  }
+}
+function displayAll() {
+  for (i in taskArray) {
+    var myTask = document.getElementById('label-' + taskArray[i].name);
+    myTask.style.display = "inline";
+  }
+}
+
+
+// ACTUALISATION LOCALSTORAGE
+function updateLocalStorage() {
+  if (taskArray.length != 0) {taskStorage.setItem('userStorage', JSON.stringify(taskArray));}
+  else {taskStorage.removeItem('userStorage');}
+}
+
+
+
+// CONVERSION MILLISECONDES EN FORMAT DATE
+function msToHMS(ms) {
+    // 1- Convert to seconds:
+    var seconds = ms / 1000;
+    // 2- Extract hours:
+    var hours = parseInt( seconds / 3600 ); // 3,600 seconds in 1 hour
+    seconds = seconds % 3600; // seconds remaining after extracting hours
+    // 3- Extract minutes:
+    var minutes = parseInt( seconds / 60 ); // 60 seconds in 1 minute
+    // 4- Keep only seconds not extracted to minutes:
+    seconds = Math.round(seconds % 60);
+    return ("Vous avez réalisé cette tâche en "+ hours+" heures, "+minutes+" minutes et "+seconds+" secondes.");
+  } 
+
+// RECHERCHE OBJET PAR PROPRIÉTÉ NOM
+function findTaskByName(nameTask) {
+  function findTask(thisTask) { 
+    return thisTask.name == nameTask;
+  }
+  return taskArray.find(findTask);
+}
+
+
+
+
+//DEV
+function log(truc) {
+  console.log(truc);
+}
+
+
